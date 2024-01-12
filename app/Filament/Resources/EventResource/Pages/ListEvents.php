83dd5f024bb308aa07e91d0970d9bdfb8ba7ac2c
Thomas Laigneau <thomas.laigneau.pro@gmail.com>
2024-01-12 10:58:50 +0100

    feat(admin): add active/inactive tabs on event

--- app/Filament/Resources/EventResource/Pages/ListEvents.php
+++ app/Filament/Resources/EventResource/Pages/ListEvents.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Resources\Pages\ListRecords;
 
 class ListEvents extends ListRecords
@@ -14,6 +16,15 @@ class ListEvents extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'active' => Tab::make()->modifyQueryUsing(fn (Builder $query) => $query->whereDate('start_date', '>=', now())),
+            'inactive' => Tab::make()->modifyQueryUsing(fn (Builder $query) => $query->whereDate('start_date', '<=', now())),
         ];
     }
 }
