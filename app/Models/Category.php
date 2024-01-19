415ab9784a487d0d59ad85961858f0d4107c8bb6
HugoDemont <hugodemont62@hotmail.fr>
2024-01-19 13:46:37 +0100

    commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
@@ -13,7 +14,7 @@ class Category extends Model
         'name'
     ];
 
-    public function events()
+    public function events(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->belongsToMany(Event::class);
     }
