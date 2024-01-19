aee7820c9ce6fd7b0c40a5bef3d471a055b95fc3
HugoDemont <hugodemont62@hotmail.fr>
2024-01-19 15:08:59 +0100

    Remove unused imports in EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
