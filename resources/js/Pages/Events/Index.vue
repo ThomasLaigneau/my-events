d656bff809baedd030d6e4e7874c6049bcbec108
HugoDemont <hugodemont62@hotmail.fr>
2024-02-02 14:04:31 +0100

    [REFACTOR] Refactor Events page, optimize code structure and readability
    Optimized the structure of the Events Index page by removing unnecessary white space and commenting out unused code. This has improved the readability and maintainability of the codebase. Also simplified logic to calculate checked checkbox count.

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -28,31 +28,20 @@ const unselectCheckbox = () => {
 
 const checkboxes = document.getElementsByClassName('myCheckbox');
 let checkedCount = 0;
-// for (const checkbox of checkboxes) {
-//     if (checkbox.checked) {
-//         checkedCount++;
-//     }
 for (let i = 0; i < myCheckbox.value.length; i++) {
     checkedCount++;
     console.log(checkedCount);
 }
-
-// }
-
 function recupererValeur() {
     var PriceFrom = document.getElementById('FilterPriceFrom');
     var valeurPriceFrom = PriceFrom.value;
     console.log('La valeur de l\'input est : ' + valeurPriceFrom);
 }
-
 const highestPrice = events => Math.max(...events);
-
-
 </script>
 
 <template>
     <AppLayout title="Events">
-
         <section>
             <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
                 <header>
@@ -62,27 +51,23 @@ const highestPrice = events => Math.max(...events);
                         Bienvenue sur notre super site d'événements!
                     </p>
                 </header>
-
                 <div class="mt-8 sm:flex sm:items-center sm:justify-between">
                     <div class="block sm:hidden">
                         <button
                             class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
                             <span class="text-sm font-medium"> Filters & Sorting </span>
-
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                  stroke="currentColor" class="h-4 w-4 rtl:rotate-180">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
                             </svg>
                         </button>
                     </div>
-
                     <div class="hidden sm:flex sm:gap-4">
                         <div class="relative">
                             <details class="group [&_summary::-webkit-details-marker]:hidden">
                                 <summary
                                     class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
                                     <span class="text-sm font-medium"> Catégories </span>
-
                                     <span class="transition group-open:-rotate-180">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                              stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
@@ -91,25 +76,21 @@ const highestPrice = events => Math.max(...events);
                                         </svg>
                                     </span>
                                 </summary>
-
                                 <div
                                     class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
                                     <div class="w-96 rounded border border-gray-200 bg-white">
                                         <header class="flex items-center justify-between p-4">
                                             <span class="text-sm text-gray-700"> {{ checkedCount }} checked </span>
-
                                             <button @click="unselectCheckbox" type="button"
                                                     class="text-sm text-gray-900 underline underline-offset-4">
                                                 Reset
                                             </button>
                                         </header>
-
                                         <ul class="space-y-1 border-t border-gray-200 p-4">
                                             <li v-for="(categories, index) in categories">
                                                 <label for="myCheckbox" class="inline-flex items-center gap-2">
                                                     <input type="checkbox" ref="myCheckbox" id="myCheckbox"
                                                            class="h-5 w-5 rounded border-gray-300"/>
-
                                                     <span class="text-sm font-medium text-gray-700"> {{
                                                             categories.name
                                                         }}
@@ -121,13 +102,11 @@ const highestPrice = events => Math.max(...events);
                                 </div>
                             </details>
                         </div>
-
                         <div class="relative">
                             <details class="group [&_summary::-webkit-details-marker]:hidden">
                                 <summary
                                     class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
                                     <span class="text-sm font-medium"> Price </span>
-
                                     <span class="transition group-open:-rotate-180">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                              stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
@@ -136,32 +115,26 @@ const highestPrice = events => Math.max(...events);
                                         </svg>
                                     </span>
                                 </summary>
-
                                 <div
                                     class="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
                                     <div class="w-96 rounded border border-gray-200 bg-white">
                                         <header class="flex items-center justify-between p-4">
                                             <span class="text-sm text-gray-700"> The highest price is {{ highestPrice }}
                                             </span>
-
                                             <button type="button"
                                                     class="text-sm text-gray-900 underline underline-offset-4">
                                                 Reset
                                             </button>
                                         </header>
-
                                         <div class="border-t border-gray-200 p-4">
                                             <div class="flex justify-between gap-4">
                                                 <label for="FilterPriceFrom" class="flex items-center gap-2">
                                                     <span class="text-sm text-gray-600">$</span>
-
                                                     <input type="number" id="FilterPriceFrom" placeholder="From"
                                                            class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"/>
                                                 </label>
-
                                                 <label for="FilterPriceTo" class="flex items-center gap-2">
                                                     <span class="text-sm text-gray-600">$</span>
-
                                                     <input type="number" id="FilterPriceTo" placeholder="To"
                                                            class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"/>
                                                 </label>
@@ -173,10 +146,8 @@ const highestPrice = events => Math.max(...events);
                         </div>
                     </div>
 
-
                     <div class="hidden sm:block">
                         <label for="SortBy" class="sr-only">SortBy</label>
-
                         <select id="SortBy" class="h-10 rounded border-gray-300 text-sm">
                             <option>Trier par</option>
                             <option value="Price, ASC">Prix croissant</option>
@@ -187,9 +158,7 @@ const highestPrice = events => Math.max(...events);
                     </div>
                 </div>
 
-
                 <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-
                     <li v-for="(event, index) in events.data">
                         <a :href="route('events.show', event)" class="block overflow-hidden">
                             <div class="group">
@@ -199,7 +168,6 @@ const highestPrice = events => Math.max(...events);
                                     {{ event.title }}
                                 </h3>
                             </div>
-
                             <div class="relative bg-white pt-3">
                                 <p>
                                     <span class="tracking-wider text-gray-900"> {{ event.price }}$</span>
